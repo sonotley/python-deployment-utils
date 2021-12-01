52ad103472277a98404f1e618ea2fcaacae676bd
Simon Notley <simonnotley@yahoo.co.uk>
2021-12-01 23:04:10 +0000

    Install script and pyproject changes

--- example_project/dist/install_on_debian.sh
+++ example_project/dist/install_on_debian.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 set -e
 echo "*************************"
-echo "Installing VMS Mimic Service"
+echo "Installing Example Project"
 echo "*************************"
-filePath=${1:-"/opt"}/vms-mimic-service
+filePath=${1:-"/opt"}/example-project
 echo "Installing to $filePath"
 echo "Building Python virtual environment"
 python3 -m venv $filePath/env
-target=(vms_mimic_service*.whl)
+target=(example_package*.whl)
 source $filePath/env/bin/activate
 pip install "${target[0]}"
-ln -s $filePath/env/bin/vms-mimic-service $filePath/vms-mimic-service
-cp -R fonts $filePath/fonts
-cp -R graphics $filePath/graphics
+ln -s $filePath/env/bin/my-executable $filePath/my-executable
 cp setup.yaml $filePath/setup.yaml
--- example_project/dist/install_on_windows.cmd
+++ example_project/dist/install_on_windows.cmd
@@ -1,19 +1,17 @@
 @echo off
 echo *************************
-echo Installing VMS Mimic Service
+echo Installing Example Project
 echo *************************
-IF %1.==. set filePath=C:\vms-mimic-service& GOTO install
-set filePath=%1\vms-mimic-service
+IF %1.==. set filePath=C:\example-project& GOTO install
+set filePath=%1\example-project
 :install
 echo Installing to %filePath%
 echo Building Python virtual environment
 py -3 -m venv %filePath%\env
-FOR /F "delims=" %%i IN ('dir /b vms_mimic_service*.whl') DO set target=%%i
+FOR /F "delims=" %%i IN ('dir /b example_package*.whl') DO set target=%%i
 call %filePath%\env\Scripts\activate
 pip install %target%
-mklink /H %filePath%\vms-mimic-service.exe %filePath%\env\Scripts\vms-mimic-service.exe
-xcopy fonts %filePath%\fonts\ /E/Q
-xcopy graphics %filePath%\graphics\ /E/Q
+mklink /H %filePath%\my-executable.exe %filePath%\env\Scripts\my-executable.exe
 xcopy setup.yaml %filePath%\ /F/Q
 echo *************************
 echo Installation complete
